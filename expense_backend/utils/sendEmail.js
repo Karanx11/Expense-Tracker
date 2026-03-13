c420cc25189102fd44987b501c4426d430354f78
unknown <karanx11.72898@gmail.com>
2026-03-13 19:52:35 +0530

    Fix Gmail IPv6 issue

--- expense_backend/utils/sendEmail.js
+++ expense_backend/utils/sendEmail.js
@@ -1,21 +1,25 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
+
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // required for port 465
+    secure: true,
+
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_PASS
     },
+
+    family: 4 // force IPv4 instead of IPv6
   });
 
   await transporter.sendMail({
     from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
     to,
     subject,
-    html,
+    html
   });
 };
 
