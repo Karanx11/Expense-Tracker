7da0d83b047c77340752689dae839718940f1f9b
unknown <karanx11.72898@gmail.com>
2026-03-13 21:22:42 +0530

    fix email smtp

--- expense_backend/utils/sendEmail.js
+++ expense_backend/utils/sendEmail.js
@@ -1,27 +1,29 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // TLS
+      family: 4,     // force IPv4
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    const info = await transporter.sendMail({
+      from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    },
-
-    family: 4 // force IPv4 (fixes Render Gmail issue)
-  });
-
-  await transporter.sendMail({
-    from: `"Expense Tracker" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
-
+    console.log("Email sent:", info.response);
+  } catch (error) {
+    console.error("Email error:", error);
+  }
 };
 
 module.exports = sendEmail;
