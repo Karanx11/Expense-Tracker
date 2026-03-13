bcc50371036bc29c412c48db0222371962464655
unknown <karanx11.72898@gmail.com>
2026-03-13 21:57:18 +0530

    Fix email module

--- expense_backend/utils/sendEmail.js
+++ expense_backend/utils/sendEmail.js
@@ -1,21 +1,30 @@
-import nodemailer from "nodemailer";
+const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
 
-  await transporter.sendMail({
-    from: `"RentKaro" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Expense Tracker" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully");
+
+  } catch (error) {
+    console.log("Email error:", error);
+  }
 };
 
-export default sendEmail;
-
+module.exports = sendEmail;
